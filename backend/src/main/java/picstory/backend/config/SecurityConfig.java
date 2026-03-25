cee60d4580f27a31bda6dfd14a9a283d9553774f
seokhyeon09 <ring4392@gmail.com>
2026-03-25 20:28:53 +0900

    로그인 백엔드

--- backend/src/main/java/picstory/backend/config/SecurityConfig.java
+++ backend/src/main/java/picstory/backend/config/SecurityConfig.java
@@ -1,24 +1,55 @@
 package picstory.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/members", "/members/{id}").permitAll()
-                        .requestMatchers("/auth/**").permitAll()
-                        //.anyRequest().authenticated()
-                        .anyRequest().permitAll()
+                                .requestMatchers("/members","/members/**").permitAll()
+                                .requestMatchers("/auth/**").permitAll()
+//                        .anyRequest().authenticated()
+                                .anyRequest().permitAll()
                 )
-                .formLogin(form -> form.disable())
+                .formLogin(form->form.disable())
                 .httpBasic(basic -> basic.disable());
+
         return http.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+
+        config.setAllowedOrigins(List.of(allowedOrigins));
+
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","PATCH"));
+
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",config);
+
+        return source;
+    }
+
 }
--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,8 +1,11 @@
 import React ,{useState}from 'react'
 import Button from '../../components/ui/Button'
-import './Login.scss'
+import './Auth.scss'
 import Input from '@/components/ui/Input'
 import { Link,useNavigate } from 'react-router-dom'
+import { login } from '@/api/auth.api'
+
+
 const Login = () => {
 
   const navigate = useNavigate()
@@ -14,14 +17,48 @@ const Login = () => {
   const [error, setError]=useState('')
   const [isLoading, setIsLoading]=useState(false)
 
+  const handleChange = (e) =>{
+    const {name, value}=e.target
+    setForm((prev)=>({
+      ...prev,
+      [name]:value
+    }))
+  }
+
+  const handleSumit=async(e)=>{
+    e.preventDefault()
+    if(!form.email.trim()){
+      setError('이메일을 입력해주세요')
+      return
+    }
+    if(!form.password.trim()){
+      setError('비밀번호를 입력해주세요')
+      return
+    }
+
+    try {
+      setIsLoading(true)
+      setError('')
+      await login({
+        email:form.email.trim(),
+        password:form.password
+      })
+      navigate('/app')
+    } catch (error) {
+      setError(error.message || '로그인을 실패했습니다.')
+    }finally{
+      setIsLoading(false)
+    }
+  }
+
   const handleBack =()=>{
     navigate(-1)
   }
 
   return (
-    <section className='login'>
+    <section className='auth'>
       <div className="inner">
-        <div className="login-box">
+        <div className="auth-box">
 
           <nav>
             <h2>로그인</h2>
@@ -30,24 +67,30 @@ const Login = () => {
             icons
             onClick={handleBack} />
           </nav>
-          <form className='login-form'>
+          <form className='auth-form' onSubmit={handleSumit}>
             <div className="form-group">
 
               <Input
                 type="email"
+                name="email"
+                value={form.email}
+                onChange={handleChange}
                 placeholder="이메일을 입력하세요"
               />
               <Input
                 type="password"
+                name="password"
+                value={form.password}
+                onChange={handleChange}
                 placeholder="비밀번호를 입력하세요"
               />
             </div>
-            <div className="login-btn-wrap">
+            <div className="auth-btn-wrap">
               <Button text="로그인" type="submit" className="primary" />
             </div>
           </form>
 
-          <div className="signup-now">
+          <div className="auth-now">
             <span>계정이 없으신가요?</span>
             <Link to="/signup">
               <Button text="회원가입하기" icons />
